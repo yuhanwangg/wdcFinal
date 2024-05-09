7cb46cc0fdcbe55e77f407cb06ebe9341271813c
georgiaMcl <a1882072@adelaide.edu.au>
2024-05-09 11:51:13 +0000

    can post

---
+++ UpdatesOrganisations.js
@@ -0,0 +1,58 @@
+function post() {
+    var updateTitleInput = document.querySelector('.newPost input[type="text"]').value;
+    var updateMessageInput = document.querySelector('.newPost textarea').value;
+
+    var oldPostsContainer = document.querySelector('.allPosts');
+
+    var newPost = document.createElement('div');
+    newPost.className = 'oldPosts';
+    var newPostContent = document.createElement('div');
+    newPostContent.className = 'postInfo';
+
+    var updateName = document.createElement('p');
+    updateName.className = 'title';
+    updateName.innerText = updateTitleInput;
+
+    var extraDetailsText = document.createElement('div');
+    extraDetailsText.className = 'extraDetails';
+
+    var OrganisationName = document.createElement('p');
+    OrganisationName.innerText = "Organiation Name";
+
+    var currentDate = new Date();
+    // Format the date and time as desired (e.g., "May 9, 2024 10:30 AM")
+    var formattedDate = currentDate.toLocaleString('en-US', {
+        month: 'long',
+        day: 'numeric',
+        year: 'numeric',
+        hour: 'numeric',
+        minute: '2-digit',
+        hour12: true
+    });
+    var postDate = document.createElement('p');
+    postDate.innerText = formattedDate;
+
+    extraDetailsText.appendChild(OrganisationName);
+    extraDetailsText.appendChild(postDate);
+
+
+    var updateInfo = document.createElement('p');
+    updateInfo.innerText = updateMessageInput;
+
+
+    newPostContent.appendChild(updateName);
+    newPostContent.appendChild(extraDetailsText);
+    newPostContent.appendChild(updateInfo);
+
+    newPost.appendChild(newPostContent);
+    var logo = document.createElement('img');
+    logo.className = 'logo';
+    logo.src = "exampleLogo.png";
+    logo.alt = "Company logo";
+
+    newPost.appendChild(logo);
+
+    oldPostsContainer.insertBefore(newPost, oldPostsContainer.firstChild);
+
+
+}
